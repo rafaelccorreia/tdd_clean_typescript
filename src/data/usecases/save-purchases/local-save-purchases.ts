2d413d78b8e038adc2083025c6797c5cc5950438
Rafael Correia <rafaelccorreia@outlook.com>
2022-11-16 16:53:18 -0300

    ensure LocalSavePurchases insert Cache with correct timestamp

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -2,10 +2,16 @@ import { CacheStore } from '@/data/protocols/cache'
 import { SavePurchase } from '@/domain/usecases'
 
 export class LocalSavePurchases implements SavePurchase {
-  constructor(private readonly cacheStore: CacheStore) {}
+  constructor(
+    private readonly cacheStore: CacheStore,
+    private readonly timestamp: Date
+  ) {}
 
   async save(purchases: Array<SavePurchase.Params>): Promise<void> {
     this.cacheStore.delete('purchases')
-    this.cacheStore.insert('purchases', purchases)
+    this.cacheStore.insert('purchases', {
+      timestamp: this.timestamp,
+      value: purchases
+    })
   }
 }
